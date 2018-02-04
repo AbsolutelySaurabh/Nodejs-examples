3ac7a85e0ceecc4c2ad826dc5573f4a63350305e
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-04 14:49:01 +0530

    node callbacks and error handling

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ var rect = require('./rectangle');
 
 function solveRect(l, b){
 	console.log("Solving for rectangle with l = " + l +" and b = " + b);
-	if( l<=0 || b<=0){
-		console.log("Rectangle dimensions should be greater than zero.");
-	}else{
-		console.log("The area of the rectangle is: " + rect.area(l, b));
-		console.log("The perimeter of the rectangle is: " + rect.perimeter(l, b));
-	}
+	
+	rect(l, b, (err, rectangle) => {
+
+		if(err){
+			console.log("ERROR: ", err.message);
+		}else{
+			
+			console.log("The area of the rectangle of dimensions l = "
+				+ l + " and b = " + b + " is: " + rectangle.area());
+			//in the abobe we do not need to give parameters is area() as it'll auto take from above method via callback
+			//in javascript due to callback the inner methos has accesss to all the variables of the outer method.
+
+			console.log("The perimeter of the rectangle of dimensions l = "
+				+ l + " and b = " + b + " is: " + rectangle.perimeter());
+			
+		}
+	});
+
+	console.log("This statement is after the call to rect().");
+
 }
 
 solveRect(2, 3);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,22 @@
-exports.perimeter = (x, y) => (2*(x+y));
-exports.area = (x, y) => (x*y);
+// exports.perimeter = (x, y) => (2*(x+y));
+// exports.area = (x, y) => (x*y);
+
+module.exports = (x, y, callback) => {
+	if(x <= 0 || y <= 0){
+
+		setTimeout(() => 
+			callback(new Error("Rectangle dimensions should be greater than zero.")
+
+				, null), 2000);
+		//created a delay of 2 sec.
+	}else{
+
+		setTimeout(() => callback(null, {
+			//here the first parameter which is error is null
+			//here is the object as 2nd par
+			perimeter: () => (2*(x+y)),
+			area: () => (x*y)
+		}),
+		2000);
+	}
+}
