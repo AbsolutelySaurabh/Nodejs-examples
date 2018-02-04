12d81074294545a1d3a5d7038c4014beaf157dbe
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-04 23:32:08 +0530

    express-router

--- index.js
+++ index.js
@@ -3,13 +3,94 @@ const http = require('http');
 
 //making use of morgan , helps in seeing the log files
 const morgan = require('morgan');
+//node router
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 //this states that the application uses express
 const app = express();
+//whenever we need to use MIDDLEWARE, use app.use()
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res, next) => {
+
+	//for all the requests, nomatter get, put, post
+	//this code will get executed by default
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	//this the next parameter callback
+	next();
+
+});
+
+//first the abpve will run then get will run
+//the req, res, next will the one above in all
+app.get('/dishes', (req, res, next) => {
+
+	//the next will cause to call it
+	res.end('Will send all the dishes to you!.');
+
+});
+
+//UPDATE
+//the req.body.name came from the jsonPrser.body() method
+app.post('/dishes', (req, res, next) => {
+	//the next will cause to call it
+	res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+//CREATE
+app.put('/dishes', (req, res, next) => {
+	//the next will cause to call it
+	res.statusCode = 403;
+	res.end('PUT operation not supported on /dishes');
+
+});
+
+//dangerous DELETE
+app.delete('/dishes', (req, res, next) => {
+	//the next will cause to call it
+	res.end('Deleting all the dishes!');
+});
+
+
+
+
+
+app.get('/dishes/:dishId', (req, res, next) => {
+
+	//the next will cause to call it
+	res.end('Will send the details of the dish: ' + req.params.dishId + ' to you!');
+
+});
+
+//CREATE
+//the req.body.name came from the jsonPrser.body() method
+app.post('/dishes/:dishId', (req, res, next) => {
+	//the next will cause to call it
+	res.statusCode = 403;
+	res.end('POST operation not supported on /dishes/ '+ req.params.dishId);
+});
+
+//UPDATE
+app.put('/dishes/:dishId', (req, res, next) => {
+
+	req.write('Updating the dish: ' + req.params.dishId + '\n');
+	res.end('Will update the dish: ' + req.body.name + 'with details: ' + req.body.description);
+
+});
+
+//dangerous DELETE
+app.delete('/dishes/:dishId', (req, res, next) => {
+	//the next will cause to call it
+	res.end('Deleting dish: ' + req.params.dishId);
+});
+
+
+
 
 //setting up the server to sunup the html files
 //after installing morgan
