be07120412285006ea8d0eeec9ba6206d8e568c0
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-04 21:34:19 +0530

    node-express

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+const express = require('express');
 const http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+//this states that the applicai
+const app = express();
+
+//the next is used when we need to invoke additional middleware, will learn further
+app.use((req, res, next) => {
 
 	console.log(req.headers);
-
-	//means everthing OK
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('<html><body><h1>Hello, World!</h1></body></html>');
+	res.end('<html><body><h1>This is an express server</h1></body></html>');
 
-})
+});
 
-//start server
+//set up 
+const server = http.createServer(app);
+
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}`);
+
+	console.log(`Server runnign at http://${hostname}:${port}`);
+
+
 });
