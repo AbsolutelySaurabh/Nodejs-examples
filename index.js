2a3955a29c5ae53150c68616c8db8c3ff594d2f2
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-04 14:34:35 +0530

    node modules

---
+++ index.js
@@ -0,0 +1,15 @@
+//here we have imported the node module
+var rect = require('./rectangle');
+
+function solveRect(l, b){
+	console.log("Solving for rectangle with l = " + l +" and b = " + b);
+	if( l<=0 || b<=0){
+		console.log("Rectangle dimensions should be greater than zero.");
+	}else{
+		console.log("The area of the rectangle is: " + rect.area(l, b));
+		console.log("The perimeter of the rectangle is: " + rect.perimeter(l, b));
+	}
+}
+
+solveRect(2, 3);
+solveRect(4, 5);
