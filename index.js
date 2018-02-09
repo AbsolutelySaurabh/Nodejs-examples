722ea3999afb794f282fb09f553a9e3628a42057
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-09 11:24:19 +0530

    callback hell and promises

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 //the databse conFusion we created earlier via terminal
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, db) => {
-
-	//the assert will check that if the "err" is null or not.
-	assert.equal(err, null);
+MongoClient.connect(url).then( db => {
 
 	console.log("Connected correctly to the server.");
 
-	//we created dishes collection previously
-	const collection = db.collection("dishes");
-	//inserting one object in collectiom dishes
-	collection.insertOne({"name": "Saurabh Singh", "description": "student"}, (err, result) => {
+	dboper.insertDocument(db, {name: "saurabh singh", description: "student"},
+	 "dishes").then( result => {
 
-			//there is nestin of calls
+		console.log("Insert doc: \n ", result.ops);
 
-			assert.equal(err, null);
+		return dboper.findDocuments(db, "dishes");
 
-			console.log("After Insert:\n");
-			//ops tells how many operations have been succesful
-			console.log(result.ops);
+		})
 
-			//empty will search for evrthing 
-			collection.find({}).toArray((err, docs) => {
+		.then(docs => {
 
-				assert.equal(err, null);
+			console.log("Found Docs: \n", docs);
 
-				console.log("Found:\n");
-				//docs will return all the docs from the collection mathcing find
-				//we can apply filter here too.
-				console.log(docs);
+			return dboper.updateDocument(db, {name: "saurabh singh"},
+			 { description: "test student"},"dishes");
 
-				//removing collections
-				db.dropCollection("dishes", (err, result) => {
+		})
 
-					assert.equal(err, null);
-					db.close();
-				});
-			});
+		.then(result => {
 
-		});
-});
+				console.log("Updates document: \n ",result.result );
+
+				dboper.findDocuments(db, "dishes");
+
+		})
+
+		.then( docs => {
+
+			console.log("Found Docs: \n", docs);
+
+			return db.dropCollection("dishes");
+
+		})
+
+		.then(result => {
+
+				console.log("Dropped colletions: ", result);
+
+				return db.close();
+
+		})
+		
+		.catch(err => console.log(err));
+
+})
+.catch(err => console.log(err));
