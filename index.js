fce5c7c2326d9503e42fdfa54e710419bb8c480c
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-09 14:15:56 +0530

    mongoose example-1

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const dboper = require('./operations');
+const mongoose = require('mongoose');
+//bluebird is a 3rd party promise module of nodejs
+mongoose.Promise = require('bluebird');
 
-//the databse conFusion we created earlier via terminal
+const Dishes = require('./models/dishes');
+
 const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
 
-MongoClient.connect(url).then( db => {
+	//this will encourage Mongo to use the MOngoDB driver to connect
+	useMongoClient: true
+});
 
-	console.log("Connected correctly to the server.");
+connect.then((db) => {
 
-	dboper.insertDocument(db, {name: "saurabh singh", description: "student"},
-	 "dishes").then( result => {
+	//when connection is istablished
+	console.log('Connected to server correctly');
 
-		console.log("Insert doc: \n ", result.ops);
+	var newDish = Dishes({
 
-		return dboper.findDocuments(db, "dishes");
+		name: 'Daal',
+		description: 'text'
+	});
 
-		})
+	newDish.save().then((dish) => {
 
-		.then(docs => {
+		console.log(dish);
 
-			console.log("Found Docs: \n", docs);
+		return Dishes.find({}).exec();
+	})
+	.then((dishes) => {
+		//when I get the dishes
+		console.log(dishes);
 
-			return dboper.updateDocument(db, {name: "saurabh singh"},
-			 { description: "test student"},"dishes");
+		return db.collection('dishes').drop();
 
-		})
+	})
+	.then(() => {
+		return db.close();
+	})
 
-		.then(result => {
+	//catch any error
+	.catch((err) => {
 
-				console.log("Updates document: \n ",result.result );
+		console.log(err);
+	});
 
-				dboper.findDocuments(db, "dishes");
-
-		})
-
-		.then( docs => {
-
-			console.log("Found Docs: \n", docs);
-
-			return db.dropCollection("dishes");
-
-		})
-
-		.then(result => {
-
-				console.log("Dropped colletions: ", result);
-
-				return db.close();
-
-		})
-		
-		.catch(err => console.log(err));
-
-})
-.catch(err => console.log(err));
+});
