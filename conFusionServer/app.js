25a6d7c76b23b363feacec6504afefbb7bd5f9f0
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-11 20:12:55 +0530

    used-basic-cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -41,32 +41,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//below is te secret-key included
+app.use(cookieParser('12345-67890-09876-54321'));
 
 //We want to do AUTH right before the queries onf our data
 
-function auth (req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-      next(); // authorized
-  } else {
+function auth (req, res, next) {
+
+  if(!req.signedCookies.user){
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+        return;
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');      
+        err.status = 401;
+        next(err);
+    }
+ }else{
+
+  //this is the second-time, so take data saved from cookie
+  if(req.signedCookies.user == 'admin'){
+    //allow
+    next();
+
+  }else{
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
       err.status = 401;
       next(err);
   }
+}
 }
 
 app.use(auth);
