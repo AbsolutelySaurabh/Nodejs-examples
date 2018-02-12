d7fe908f04790192fd874a911dddd54a99a1b744
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-13 01:40:53 +0530

    express-session-paer-1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,6 +15,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect( url, {
@@ -46,11 +48,20 @@ app.use(cookieParser('12345-67890-09876-54321'));
 
 //We want to do AUTH right before the queries onf our data
 
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
 
 function auth (req, res, next) {
 
-  if(!req.signedCookies.user){
-    console.log(req.headers);
+  console.log(req.session);
+
+  if(!req.session.user){
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -64,6 +75,8 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
+
+        req.session.user = 'admin';
         next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
@@ -74,8 +87,9 @@ function auth (req, res, next) {
  }else{
 
   //this is the second-time, so take data saved from cookie
-  if(req.signedCookies.user == 'admin'){
+  if(req.session.user == 'admin'){
     //allow
+    console.log('req.session: ', req.session);
     next();
 
   }else{
